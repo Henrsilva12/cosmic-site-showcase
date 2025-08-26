477ea87e78027f003674ca173c72e54ac4424cd7
Natanael9999 <118235069+Natanael9999@users.noreply.github.com>
2025-08-25 22:08:42 -0300

    criação da tela de equipes e adição nas rotas do site

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import ProjectLoop from "./pages/ProjectLoop";
 import ProjectGameOn from "./pages/ProjectGameOn";
 import ProjectRobotica from "./pages/ProjectRobotica";
 import ProjectRecicla from "./pages/ProjectRecicla";
+import Equipe from "./pages/Equipe";
 
 const queryClient = new QueryClient();
 
@@ -28,6 +29,7 @@ const App = () => (
             <Route path="/project-game-on" element={<ProjectGameOn />} />
             <Route path="/project-robotica" element={<ProjectRobotica />} />
             <Route path="/project-recicla" element={<ProjectRecicla />} />
+            <Route path="/equipe" element={<Equipe />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
