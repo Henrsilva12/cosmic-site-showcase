b55ba2edfbbebd5a58922a33c18ae505a98ad86f
TavaresASantos <tavaresamandasantos@gmail.com>
2025-08-27 00:38:09 -0300

    feat: Adiciona ModalProvider e integra com o Footer para envio de mensagens

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "next-themes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ModalProvider } from "@/components/ModalProvider";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Projects from "./pages/Projects";
@@ -22,16 +24,18 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/project-loop" element={<ProjectLoop />} />
-            <Route path="/project-game-on" element={<ProjectGameOn />} />
-            <Route path="/project-robotica" element={<ProjectRobotica />} />
-            <Route path="/project-recicla" element={<ProjectRecicla />} />
-            <Route path="/equipe" element={<Equipe />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+          <ModalProvider>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/project-loop" element={<ProjectLoop />} />
+              <Route path="/project-game-on" element={<ProjectGameOn />} />
+              <Route path="/project-robotica" element={<ProjectRobotica />} />
+              <Route path="/project-recicla" element={<ProjectRecicla />} />
+              <Route path="/equipe" element={<Equipe />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </ModalProvider>
         </BrowserRouter>
       </TooltipProvider>
     </ThemeProvider>
